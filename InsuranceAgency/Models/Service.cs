d573143d65b3bc8ba5b6fef9d324fec0e491edf8
rpp <rpp2182@gmail.com>
2024-11-21 19:29:33 +0300

    Добавлен Service

---
+++ InsuranceAgency/Models/Service.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace InsuranceAgency.Models
+{
+    [Table("Services")]
+    public class Service
+    {
+        [Key] public int Id { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string InsuranceObjectType { get; set; }
+        [Required]
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal PremiumCoef {  get; set; }
+        [Required]
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal PaymentCoef { get; set;}
+    }
+}
