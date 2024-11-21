05f4cc27a167c400e2b5d6f919dec2983831240d
rpp <rpp2182@gmail.com>
2024-11-21 19:25:31 +0300

    Добавлен InsuranceObject

---
+++ InsuranceAgency/Models/InsuranceObject.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace InsuranceAgency.Models
+{
+    [Table("InsuranceObjects")]
+    public class InsuranceObject
+    {
+        [Key]
+        public int Id {  get; set; }
+        [Required]
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Price {  get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Type { get; set; }
+    }
+}
