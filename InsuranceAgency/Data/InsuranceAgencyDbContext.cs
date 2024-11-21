781ecf49c3a7d629c268417de2c45ff0f81ee924
rpp <rpp2182@gmail.com>
2024-11-21 20:12:52 +0300

    Добавлен DbContext

---
+++ InsuranceAgency/Data/InsuranceAgencyDbContext.cs
@@ -0,0 +1,20 @@
+﻿using InsuranceAgency.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace InsuranceAgency.Data
+{
+    public class InsuranceAgencyDbContext:DbContext
+    {
+        public InsuranceAgencyDbContext(DbContextOptions<InsuranceAgencyDbContext> options) : base(options) { }
+
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Administrator> Administrators { get; set; }
+        public DbSet<Accountant> Accountants { get; set; }
+        public DbSet<InsuranceAgent> InsuranceAgents { get; set; }
+        public DbSet<InsuranceObject> InsuranceObjects { get; set; }
+        public DbSet<Service> Services { get; set; }
+        public DbSet<PolicyClaim> PolicyClaims { get; set; }
+        public DbSet<PaymentClaim> PaymentClaims { get; set; }
+        public DbSet<Policy> Policies { get; set; }
+    }
+}
--- InsuranceAgency/Program.cs
+++ InsuranceAgency/Program.cs
@@ -1,8 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+string connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
+builder.Services.AddDbContext<PolicyDbContext>(options =>
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
