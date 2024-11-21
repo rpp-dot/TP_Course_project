329fbea908f9d58548f650ed5e383f24736ceb02
rpp <rpp2182@gmail.com>
2024-11-21 18:53:47 +0300

    Добавлен Employee

---
+++ InsuranceAgency/Models/Employee.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace InsuranceAgency.Models
+{
+    public abstract class Employee:User
+    {
+        [Required]
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Salary {  get; set; }
+    }
+}
