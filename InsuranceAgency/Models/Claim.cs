2a263f85c375f0806582626191340ddf40463c2e
rpp <rpp2182@gmail.com>
2024-11-21 19:18:45 +0300

    Добавлен Claim

---
+++ InsuranceAgency/Models/Claim.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace InsuranceAgency.Models
+{
+    public abstract class Claim
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(200)]
+        public string Description { get; set; }
+        [Required]
+        [Column(TypeName = "DATE")]
+        public DateOnly ClaimDate { get; set; }
+        [Required]
+        public ClaimStatus ClaimStatus { get; set; }
+    }
+    public enum ClaimStatus
+    {
+        Создана = 1,
+        Обрабатывается = 2,
+        Одобрена = 3,
+        Отклонена = 4
+    }
+}
